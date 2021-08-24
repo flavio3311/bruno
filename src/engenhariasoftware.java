e1ffa50388a30aea984e9ad0c03efc27d8e50b43
flavio3311 <flaviomikhailgaldino2003@gmail.com>
2021-08-24 17:00:51 -0300

    adicionando soma

--- src/engenhariasoftware.java
+++ src/engenhariasoftware.java
@@ -1,4 +1,14 @@
 
 public class engenhariasoftware {
+	public static void main(String args[]) {
+		int num1 = 10;
+	    int num2 = 5;
+	    int num3 = 9;
+	    
+	    System.out.println(" O resultado da soma é" +num1+num2);
+	    
+	    
+	    
+	}
 
 }
