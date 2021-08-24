d62837341094843627811fb75607358fa63da6ac
flavio3311 <flaviomikhailgaldino2003@gmail.com>
2021-08-24 17:04:45 -0300

    adicionando multiplicacao

--- src/engenhariasoftware.java
+++ src/engenhariasoftware.java
@@ -5,7 +5,8 @@ public class engenhariasoftware {
 	    int num2 = 5;
 	    int num3 = 9;
 	    
-	    System.out.println(" O resultado da soma é" +num1+num2);
+	    System.out.println(" O resultado da soma é " +num1+num2);
+	    System.out.println("o resultado da multiplicaçao é " +num1*num3);
 	    
 	    
 	    
